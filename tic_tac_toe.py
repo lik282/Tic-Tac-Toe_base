198af73ec0d5a0eaa3f179841fa75181afdeeb50
lik282 <tsz.tothszabolcs@gmail.com>
2025-04-27 21:25:03 +0200

    a

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -14,6 +14,37 @@ def main():
     is_game_running = True
     while is_game_running:
         display_board(board)
+    
+    game_mode = get_menu_option()  # Get the game mode
+    board = get_empty_board()  # Create an empty board
+    current_player = 'X'  # Start with player 'X'
+    is_game_running = True  # Flag to control the game loop
+    
+    while is_game_running:
+        display_board(board)  # Display the current state of the board
+        
+        # Select coordinates based on game mode and current player
+        if current_player == 'X':  # Player X is always human
+            if game_mode == HUMAN_VS_HUMAN:
+                x, y = get_human_coordinates(board, current_player)
+            elif game_mode == HUMAN_VS_RANDOM_AI:
+                x, y = get_human_coordinates(board, current_player)
+            elif game_mode == HUMAN_VS_UNBEATABLE_AI:
+                x, y = get_human_coordinates(board, current_player)
+        elif current_player == 'O':  # Player O is AI
+            if game_mode == RANDOM_AI_VS_RANDOM_AI or game_mode == HUMAN_VS_RANDOM_AI:
+                x, y = get_random_ai_coordinates(board, current_player)
+            elif game_mode == HUMAN_VS_UNBEATABLE_AI:
+                x, y = get_unbeatable_ai_coordinates(board, current_player)
+        
+        # Make the move on the board
+        board[x][y] = current_player
+        
+        
+        # Check for winning or tie
+        
+        winning_player = get_winning_player(board)
+        its_a_tie = is_board_full(board)
         
         ### TO DO ###
         # in each new iteration of the while loop the program should 
